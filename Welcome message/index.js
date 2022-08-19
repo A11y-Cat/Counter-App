a2d4401f6454b56e9db618a804c3cfb00a4f8399
A11y-Cat <91890089+A11y-Cat@users.noreply.github.com>
2022-08-19 15:43:02 +0100

    added counter/save/string

--- Welcome message/index.js
+++
@@ -1,8 +0,0 @@
-welcomeEl = document.getElementById("welcome-el")  //grabs from html doc
-
-let nme = "Allison"
-let greeting = "Welcome Back "
-
-welcomeEl.innerText = greeting + nme
-
-welcomeEl.innerText += '👋'  //adds emoji to the code above
