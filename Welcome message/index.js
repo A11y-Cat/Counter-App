83067ddd5cb97dfde073297d9431f796c99c8b46
A11y-Cat <91890089+A11y-Cat@users.noreply.github.com>
2022-08-19 15:36:13 +0100

    finished app

---
+++ Welcome message/index.js
@@ -0,0 +1,8 @@
+welcomeEl = document.getElementById("welcome-el")  //grabs from html doc
+
+let nme = "Allison"
+let greeting = "Welcome Back "
+
+welcomeEl.innerText = greeting + nme
+
+welcomeEl.innerText += '👋'  //adds emoji to the code above
